9ed5ea8e3dd9539d47594b9f50cb882999f8ca85
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-10-25 23:15:01 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -10,5 +10,19 @@ let isBeginner: boolean = true;
 let name: string = 'Jonathan';
 let sentence: string = `My name is ${name} I am a beginner in TypeScript`;
 
+//n is type null
+let n: null = null;
 
+//u is type undefined
+let u: undefined = undefined;
+
+//isNew is type boolean
+let isNew: boolean = null;
+
+//myName is type string.
+let myName: string = undefined
+//console.log(sentence);
+
+let list1:number[] = [1,2,3];
+let list2: Array<number> = [1,2,3];
 
