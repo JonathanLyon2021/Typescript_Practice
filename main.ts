bf2f60fb5460b78a34c249d0058455ffc374e1e5
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-10-25 23:13:06 -0400

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,14 @@
+export{};
+let message = 'Hello Jonathan';
+console.log(message);
+
+let x = 10;
+const y = 20;
+
+let isBeginner: boolean = true;
+//let isBeginner is type boolean and is assigned the value true
+let name: string = 'Jonathan';
+let sentence: string = `My name is ${name} I am a beginner in TypeScript`;
+
+
+
