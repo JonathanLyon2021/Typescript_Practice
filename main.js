ef23a7e2dc5545101e24a7c074b39fdbf9635004
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-10-25 23:15:26 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var message = 'Hello Jonathan';
+console.log(message);
+var x = 10;
+var y = 20;
+var isBeginner = true;
+//let isBeginner is type boolean and is assigned the value true
+var name = 'Jonathan';
+var sentence = "My name is ".concat(name, " I am a beginner in TypeScript");
+var n = null;
+var u = undefined;
+var isNew = null;
+var myName = undefined;
+//console.log(sentence);
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
