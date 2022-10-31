7d15a65094196357d41e7d6d46ab05c2c3d8c119
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-10-30 21:57:11 -0400

    Error Mssg - line 9
    We have to initialize const variables in Typescript. This results in an error.

---
+++ main2.ts
@@ -0,0 +1,9 @@
+export {}
+let message = 'Welcome back Jonathan';
+console.log(message); // Welcome back Jonathan
+
+let x = 10;
+const y = 20; // const is a constant variable
+
+let sum; // by default it is of type any
+const title; // error: const variables must be initialized
