fdb383a71ff62ee00e34831a20e417bffd4d9e2b
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-10-31 07:30:06 -0400

    Update main2.ts

--- main2.ts
+++ main2.ts
@@ -6,4 +6,9 @@ let x = 10;
 const y = 20; // const is a constant variable
 
 let sum; // by default it is of type any
-const title; // error: const variables must be initialized
+
+const title = 'Codevolution'; // by default it is of type string
+
+let isBeginner: boolean = true; // by default it is of type boolean w/o the datatype declaration
+let total: number = 0;
+let name: string = 'JonBoy'; 
